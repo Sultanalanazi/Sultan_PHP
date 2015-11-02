c7ec4c57710c21f362c08463f3177a0cfb7afa7b
1310746 <sult8@hotmail.com>
2015-11-02 17:11:51 +0000

    mnn

--- switch.php
+++ switch.php
@@ -5,8 +5,13 @@ echo $myArray[0]; // outputs "do"
 
 $myArray[1] = "la"; // modifies position 1 (re)
 
+//"do", "la", "re", "mi",
 echo $myArray[1]; // outputs "la"
 
 unset($myArray[2]); // removes the array in position 2
+
+foreach($myArray as $x) {
+    print "<p>$x</p>";
+}
 
 ?>
