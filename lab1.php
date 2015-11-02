8147ab2f119c232b369b01ea7d07366344c7c3e0
1310746 <sult8@hotmail.com>
2015-11-02 14:42:22 +0000

    mnn

--- lab1.php
+++ lab1.php
@@ -5,11 +5,22 @@ $age = 19;
 if ($age >= 16) {
     print "<p>You are allowed to buy specs";
 }
-
-if ($age >= 18){
-        print "<p>you are allowed to buy mugs";
+else {
+    print "<p>You are not allowed to buy specs</p>";
 }
-if ($age >= 21){
-         print"<p>you are allowed to buy sausage rolls";
+
+if ($age >= 18) {
+    print "<p>you are allowed to buy mugs";
+}
+else {
+        print "<p>You are not allowed to buy mugs</p>";
+
+}
+if ($age >= 21) {
+    print"<p>you are allowed to buy sausage rolls";
+}
+else {
+        print "<p>You are not allowed to buy sausage rolls</p>";
+
 }
 ?>
