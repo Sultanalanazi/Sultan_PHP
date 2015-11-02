49eb1a10bdeadbc396a1c82df3e34f46858fecfb
1310746 <sult8@hotmail.com>
2015-11-02 17:04:53 +0000

    mnn

--- switch.php
+++ switch.php
@@ -1,17 +1,12 @@
 <?php
-$numberOfHobbits = 2;
+$myArray = array("do", "re", "mi"); // declares the array
 
-switch ($numberOfHobbits) {
-    case 1:
-        echo "1 sad hobbit";
-        break;
-    case 2:
-        echo "2 happy hobbits";
-        break;
-    case 3:
-        echo "3 hobbits are a crowd";
-        break;
-    default:
-        echo "All the hobbits have gone home";
-}
+echo $myArray[0]; // outputs "do"
+
+$myArray[1] = "la"; // modifies position 1 (re)
+
+echo $myArray[1]; // outputs "la"
+
+unset($myArray[2]); // removes the array in position 2
+
 ?>
