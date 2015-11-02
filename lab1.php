cade272b21f92417ee6ac364a116982e5a65611c
1310746 <sult8@hotmail.com>
2015-11-02 15:12:56 +0000

    mnn

--- lab1.php
+++ lab1.php
@@ -23,4 +23,21 @@ else {
         print "<p>You are not allowed to buy sausage rolls</p>";
 
 }
+
+$numberOfHobbits = 2;
+
+switch ($numberOfHobbits) {
+    case 1:
+        echo "1 sad hobbit";
+        break;
+    case 2:
+        echo "2 happy hobbits";
+        break;
+    case 3:
+        echo "3 hobbits are a crowd";
+        break;
+    default:
+        echo "All the hobbits have gone home";
+}
+
 ?>
