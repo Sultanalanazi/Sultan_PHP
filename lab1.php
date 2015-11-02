38fdd767ec3d172a622683c1112f21c151cd13d3
1310746 <sult8@hotmail.com>
2015-11-02 14:31:24 +0000

    mnn

--- lab1.php
+++ lab1.php
@@ -2,14 +2,14 @@
 
 $age = 19;
 
-if ($age >= 16){
+if ($age >= 16) {
     print "<p>You are allowed to buy specs";
 }
 
-if {$age >= 18){
+if ($age >= 18){
         print "<p>you are allowed to buy mugs";
 }
-if {$age >= 21){
+if ($age >= 21){
          print"<p>you are allowed to buy sausage rolls";
 }
 ?>
