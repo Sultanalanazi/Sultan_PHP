37172bd4d0123102916e705d871e8b015e84696c
1310746 <sult8@hotmail.com>
2015-11-02 14:29:41 +0000

    mnn

--- lab1.php
+++ lab1.php
@@ -1,11 +1,15 @@
 <?php
 
-  $name = "Simon";
+$age = 19;
 
-  if ($name == "Simon") {
-      print "I know you!";
-  }
-  else {
-      print "Who are you?";
-  }
+if ($age >= 16){
+    print "<p>You are allowed to buy specs";
+}
+
+if {$age >= 18){
+        print "<p>you are allowed to buy mugs";
+}
+if {$age >= 21){
+         print"<p>you are allowed to buy sausage rolls";
+}
 ?>
